d05c576e0799c48d09e0c0916a737ad4ef09ab5d
Asela0522 <aselarohana0522@gmail.com>
2023-09-13 14:44:30 +0530

    Learn python Variable

---
+++ Basic recap.py
@@ -0,0 +1,26 @@
+# Today i practice the python basic 
+print("Hello World!")
+x=10
+y=3
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x%y)
+print(type(x))
+A="Asela Rohana"
+B=23
+C=12.34
+D=True
+E=False
+F=0+5J
+print(type(A))
+print(type(B))
+print(type(C))
+print(type(D))
+print(type(E))
+print(type(F))
+
+Name=input("Enter your Name:")
+print("Your name is "+Name)
